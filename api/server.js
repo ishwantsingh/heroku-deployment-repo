14ccd66129f4a3da80938c814df0bfc415a21e7c
Ishwant Singh <ishwant8@gmail.com>
2019-04-05 15:52:58 +0530

    shoutouts corrected

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ server.get("/", async (req, res) => {
   try {
     const mot = process.env.MOT || "No Message";
     const shoutouts = await db("shoutouts");
-    res.status(200).json({ messageOfTheDay: mot, shoutouts });
+    res.status(200).json({ messageOfTheDay: mot, shoutouts: shoutouts });
   } catch (error) {
     console.error("\nERROR", error);
     res.status(500).json({ error: "Cannot retrieve the shoutouts" });
