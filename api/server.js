544d0ae62f29f8d894e67a382b94c54795660499
Ishwant Singh <ishwant8@gmail.com>
2019-04-05 15:51:00 +0530

    adds mot

--- api/server.js
+++ api/server.js
@@ -10,8 +10,9 @@ server.use(express.json());
 
 server.get("/", async (req, res) => {
   try {
+    const mot = process.env.MOT || "No Message";
     const shoutouts = await db("shoutouts");
-    res.status(200).json(shoutouts);
+    res.status(200).json({ messageOfTheDay: mot, shoutouts });
   } catch (error) {
     console.error("\nERROR", error);
     res.status(500).json({ error: "Cannot retrieve the shoutouts" });
